ad6a06a0a18a5e7e4ecea462d429dbeb805ebcad
postfull <saeful.mahmud@gmail.com>
2020-01-18 16:41:22 +0700

    Add project files.

---
+++ MVCPemula/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCPemula.MvcApplication" Language="C#" %>
---
+++ MVCPemula/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCPemula.Startup))]
+namespace MVCPemula
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
