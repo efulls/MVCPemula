8ee8ae9c85b81f811b00831721faba8c435db47d
postfull <saeful.mahmud@gmail.com>
2020-01-18 20:29:57 +0700

    Pembuatan Model Pegawai menggunakan Entity Framework, serta Pembuatan ViewModel Pegawai dan Login

---
+++ PengelolaanPegawai.Web/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PengelolaanPegawai.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { set; get; }
+        public string Password { set; get; }
+        public bool IsRemember { set; get; }
+    }
+}
